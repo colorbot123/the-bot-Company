c2fb4380402d02c3b63f2c98441faf20c6de0cbd
asdasdzen <zzen54585@gmail.com>
2024-10-31 15:28:49 +0300

    the bot Company

---
+++ index.js
@@ -0,0 +1,17 @@
+let eeeeee
+
+
+
+document.getElementById('feedback').onclick = function(){
+  eeeeee = document.getElementById('feedback1').value;
+
+
+document.getElementById('ha').textContent = `(Thank you  Give us feedback (${eeeeee}))`
+
+
+
+
+
+
+
+}
